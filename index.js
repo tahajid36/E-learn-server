1294669f4ee67f93dc515a1b2c3e77489d18596c
tahajid36 <brucextahajid@gmail.com>
2025-11-27 13:48:19 +0600

    deployment check

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
-const app = express()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-// middlewares 
-app.use(express.json())
-app.use(cors())
+// middlewares
+app.use(express.json());
+app.use(cors());
 
+const port = 9000;
 
-
-const port = 9000
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@bruce.s9kj5xo.mongodb.net/?appName=Bruce`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@bruce.s9kj5xo.mongodb.net/?appName=Bruce`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -21,37 +19,51 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("elearn");
     const courseCollection = db.collection("courses");
 
-    app.get('/courses',async (req, res) => {
-        const result = await courseCollection.find().toArray()
-        res.send(result)
-      })
+    app.get("/courses", async (req, res) => {
+      const result = await courseCollection.find().toArray();
+      res.send(result);
+    });
+    // api to get single course data for details page 
+    app.get("/courses/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await courseCollection.findOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+// api to add new courses to existing collection and show on ui 
+    app.post('courses', async (req, res)=>{
+      const body = req.body 
+      const result = await courseCollection.insertOne(body)
+      res.send(result)
+    })
 
 
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // await client.close();
   }
 }
 run().catch(console.dir);
 
-app.get('/', (req,res)=>{
-    const result = 'Hello world'
-    res.send(result)
-})
+app.get("/", (req, res) => {
+  const result = "Hello world";
+  res.send(result);
+});
 
 app.listen(port, () => {
-  console.log(`elearn server running on port ${port}`)
-})
+  console.log(`elearn server running on port ${port}`);
+});
